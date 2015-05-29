6a1707e5c391e6aa49a0d79cebef57a0b1386186
calam1 <chrislam67@gmail.com>
2015-05-29 16:05:11 -0500

    initial commit

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like tuping this."
+puts "this is fun."
+puts "Yay, Priniing"
+puts "I'd must rathr you 'not'"
+#puts 'I "said" do not touch this'
+puts "print another line"
